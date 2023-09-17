5ba6ee7dc4d02efd186f6f35234e2e164fd1e798
hamzahdili2001 <hdilihamza955@gmail.com>
2023-09-17 19:33:54 +0100

    add -y flag to flatpak instalation

--- app_install.sh
+++ app_install.sh
@@ -8,15 +8,16 @@ sudo usermod --append --groups libvirt $USER
 sudo wget https://download.virtualbox.org/virtualbox/7.0.10/virtualbox-7.0_7.0.10-158379~Debian~bookworm_amd64.deb ~/Downloads/
 sudo dpkg -i ~/Downloads/virtualbox-7.0_7.0.10-158379~Debian~bookworm_amd64.deb
 # flatpak
-sudo flatpak install flathub com.google.Chrome
-sudo flatpak install flathub com.brave.Browser
-sudo flatpak install flathub com.discordapp.Discord
-sudo flatpak install flathub com.visualstudio.code
-sudo flatpak install flathub org.mozilla.Thunderbird
-sudo flatpak install flathub com.spotify.Client
-sudo nala remove firefox-esr
-sudo flatpak install flathub org.mozilla.firefox
-sudo flatpak install flathub org.telegram.desktop
-sudo flatpak install flathub com.mattjakeman.ExtensionManager
-sudo flatpak install flathub com.getpostman.Postman
-sudo flatpak install flathub org.gnome.Boxes
+sudo flatpak install flathub com.google.Chrome -y
+sudo flatpak install flathub com.brave.Browser -y
+sudo flatpak install flathub com.discordapp.Discord -y
+sudo flatpak install flathub com.visualstudio.code -y
+sudo flatpak install flathub org.mozilla.Thunderbird -y
+sudo flatpak install flathub com.spotify.Client -y
+sudo apt remove firefox-esr -y
+sudo flatpak install flathub org.mozilla.firefox -y
+sudo flatpak install flathub org.telegram.desktop -y
+sudo flatpak install flathub com.mattjakeman.ExtensionManager -y
+sudo flatpak install flathub com.getpostman.Postman -y
+sudo flatpak install flathub org.gnome.Boxes -y
+sudo flatpak install flathub tv.kodi.Kodi -y
