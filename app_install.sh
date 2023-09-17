1748d4763ed3a1adf25331fa1f1d5da883e110aa
hamzahdili2001 <hdilihamza955@gmail.com>
2023-09-17 19:25:00 +0100

    add more apps to install

--- app_install.sh
+++ app_install.sh
@@ -2,12 +2,21 @@
 
 
 # debian repo
-sudo nala install libavcodec-extra vlc synaptic
+sudo nala install libavcodec-extra vlc synaptic valgrind -y 
+sudo apt install vagrant-libvirt libvirt-daemon-system
+sudo usermod --append --groups libvirt $USER
+sudo wget https://download.virtualbox.org/virtualbox/7.0.10/virtualbox-7.0_7.0.10-158379~Debian~bookworm_amd64.deb ~/Downloads/
+sudo dpkg -i ~/Downloads/virtualbox-7.0_7.0.10-158379~Debian~bookworm_amd64.deb
 # flatpak
 sudo flatpak install flathub com.google.Chrome
 sudo flatpak install flathub com.brave.Browser
 sudo flatpak install flathub com.discordapp.Discord
 sudo flatpak install flathub com.visualstudio.code
 sudo flatpak install flathub org.mozilla.Thunderbird
-flatpak install flathub com.spotify.Client
-
+sudo flatpak install flathub com.spotify.Client
+sudo nala remove firefox-esr
+sudo flatpak install flathub org.mozilla.firefox
+sudo flatpak install flathub org.telegram.desktop
+sudo flatpak install flathub com.mattjakeman.ExtensionManager
+sudo flatpak install flathub com.getpostman.Postman
+sudo flatpak install flathub org.gnome.Boxes
